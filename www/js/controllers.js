b04fe51b65db59faa270aa04709aeee1b4932984
Trazius Biz <traziusbiz@gmail.com>
2016-07-08 22:27:28 +0300

    replace html5 geoloc to cordova plugin

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,47 +1,46 @@
 angular.module('starter')
 
-  .controller('mainCtrl', function ($scope, $http, $ionicLoading) {
+  .controller('mainCtrl', function ($scope, $http, $ionicLoading, $cordovaGeolocation, $interval) {
     var Api_Key = '8f52968e46bd20e291e0223d691f88da';
-
 
     $scope.update = function () {
       $ionicLoading.show();
-      var pos;
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-          pos = {
-            lat: position.coords.latitude,
-            lon: position.coords.longitude
-          };
+
+      var posOptions = {timeout: 10000, enableHighAccuracy: false};
+      $cordovaGeolocation
+        .getCurrentPosition(posOptions)
+        .then(function (position) {
+          var lat  = position.coords.latitude;
+          var lon = position.coords.longitude;
 
           $http({
             method: 'GET',
             url: 'http://api.openweathermap.org/data/2.5/weather',
             params: {
               APPID: Api_Key,
-              lat: pos.lat,
-              lon: pos.lon,
+              lat: lat,
+              lon: lon,
               units: 'metric'
             }
           })
             .then(function (resp) {
               $scope.weather = resp.data;
-              console.log(resp.data)
-              $ionicLoading.hide()
+              console.log(resp.data);
+              $ionicLoading.hide();
             })
-        }, function () {
-          handleLocationError(true, infoWindow, map.getCenter());
+        }, function(err) {
+          alert('Что-то пошло не так (((')
+          $ionicLoading.hide();
+          $scope.weather = 'Что-то пошло не так (((';
         });
-      } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-      }
+    };
 
-
-    }
     $scope.update();
 
-
+    $interval(function () {
+      $scope.update();
+    }, 600*1000)
+    
   })
 
 
