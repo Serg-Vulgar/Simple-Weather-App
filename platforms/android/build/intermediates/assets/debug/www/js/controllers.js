2b78f9602330dbe5cc3164c7dcc3b0b49af2b4ee
Trazius Biz <traziusbiz@gmail.com>
2016-07-09 13:22:28 +0300

    work

--- platforms/android/build/intermediates/assets/debug/www/js/controllers.js
+++ platforms/android/build/intermediates/assets/debug/www/js/controllers.js
@@ -1,46 +1,53 @@
 angular.module('starter')
 
-  .controller('mainCtrl', function ($scope, $http, $ionicLoading, $cordovaGeolocation) {
-    var Api_Key = '8f52968e46bd20e291e0223d691f88da';
+.controller('mainCtrl', function ($scope, $http, $ionicLoading, $cordovaGeolocation, $interval) {
+  var Api_Key = '8f52968e46bd20e291e0223d691f88da';
 
+  $scope.update = function () {
+      // $ionicLoading.show();
 
+      var posOptions = {
+        timeout: 10000,
+        enableHighAccuracy: true
+      };
+      $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        // $http({
+        //   method: 'GET',
+        //   url: 'http://api.openweathermap.org/data/2.5/weather',
+        //   params: {
+        //     'appid': Api_Key,
+        //     'lat': lat,
+        //     'lon': long,
+        //     'units': 'metric'
+        //   }
+        // })
+        $http({
+          method: 'GET',
+          url: 'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=8f52968e46bd20e291e0223d691f88da'
 
-    $scope.update = function () {
-      $ionicLoading.show();
-
-      var posOptions = {timeout: 10000, enableHighAccuracy: false};
-      $cordovaGeolocation
-        .getCurrentPosition(posOptions)
-        .then(function (position) {
-          var lat  = position.coords.latitude;
-          var lon = position.coords.longitude;
-
-          $http({
-            method: 'GET',
-            url: 'http://api.openweathermap.org/data/2.5/weather',
-            params: {
-              APPID: Api_Key,
-              lat: lat,
-              lon: lon,
-              units: 'metric'
-            }
-          })
-            .then(function (resp) {
-              $scope.weather = resp.data;
-              console.log(resp.data)
-              $ionicLoading.hide()
-            })
-        }, function(err) {
-          alert('Что-то пошло не так (((')
+        })
+        .then(function (resp) {
+          $scope.weather = resp.data;
+          alert('resp', resp.data);
+          // $ionicLoading.hide();
+        }, function(err){
+          alert('err', err.data);
+          $scope.error = err;
         });
-
-
-
-
-    }
+      }, function(err) {
+        alert('Что-то пошло не так (((');
+        // $ionicLoading.hide();
+        $scope.weather = 'Что-то пошло не так (((';
+      });
+    };
 
     $scope.update();
 
+    $interval(function () {
+      $scope.update();
+    }, 600*1000);
 
   })
 
