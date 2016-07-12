f92b744df1033b562988b9b99c0096f7ae774808
Igor Ilchenko <wog516@gmail.com>
2016-07-12 17:41:05 +0300

    add graphic, gradients, days

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $state) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -20,6 +20,7 @@ angular.module('starter', ['ionic', 'ngCordova'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+    $state.transitionTo('main');
   });
 })
 
@@ -28,7 +29,13 @@ angular.module('starter', ['ionic', 'ngCordova'])
   $stateProvider
     .state('main', {
       url: "/",
-      templateUrl: "templates/main.html"
-    });
+      templateUrl: "templates/main.html",
+      controller: 'mainCtrl'
+    })
+  .state('map', {
+    url: "/map",
+    templateUrl: "templates/weather-map.html",
+    controller: 'mapCtrl'
+  });
   $urlRouterProvider.otherwise("/");
 })
